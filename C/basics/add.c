34cab3e088d7a93cb7ad3903067dcf3e15b60a7e
Ayush_Pundir <ayushpundir11@gmail.com>
2025-04-07 18:18:50 +0530

    added 2 basic code written in C language

---
+++ C/basics/add.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+
+int main(){
+    int a = 21;
+    int b = 9;
+    printf("the sum of two numbers is %d", a+b);// %d is format specifire
+    return 0;
+}
---
+++ C/basics/area_square.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int side;
+    printf("Enter side a:");
+    scanf("%d", &side);
+    printf("The area of square is %f\n", pow(side,2));//pow double,,,
+    return 0;// ,,,value return karta ha
+}
