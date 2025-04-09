dcecb32ecb7976ab5d77fbfe95e78ca5c061178f
Ayush_Pundir <ayushpundir11@gmail.com>
2025-04-09 14:45:31 +0530

    list methods

---
+++ Java/basics/Listmethods.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Listmethods {
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(6);
+        list.add(83);
+
+        System.out.println(list.indexOf(6));
+
+
+    }
+}
